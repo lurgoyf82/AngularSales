cb2a950f29a0c9f3bd4a0e9f245f16431f2d7107
Raphael <lurgoyf82@hotmail.com>
2025-06-23 13:44:09 +0200

    Configure root route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CartFormComponent } from './cart-form/cart-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CartFormComponent },
+];
