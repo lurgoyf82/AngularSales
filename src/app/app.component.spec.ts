dd97bf723f30003d48e5353ece40b5776ba317fa
Raphael <lurgoyf82@hotmail.com>
2025-06-25 16:06:20 +0200

    chore: use withFetch HTTP client

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-      providers: [provideHttpClient()]
+      providers: [provideHttpClient(withFetch())]
     }).compileComponents();
   });
 
