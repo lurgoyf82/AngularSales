67d6ef508279ce275f954f266cb2ac692a6ab042
Raphael <lurgoyf82@hotmail.com>
2025-06-28 11:16:17 +0200

    Add sidenav layout and integrate into app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CartFormComponent } from './components/cart-form/cart-form.component';
 import { ToolbarComponent } from './layout/toolbar/toolbar.component';
+import { SidenavComponent } from './layout/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CartFormComponent, ToolbarComponent],
+  imports: [RouterOutlet, CartFormComponent, ToolbarComponent, SidenavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
